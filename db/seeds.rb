9f22e430885b420e73ae11257590d8e7eda244b4
thunderenlight <thunderenlight@gmail.com>
2016-05-28 14:37:43 -0500

    Unwrapping the ruby wrappers for Eventbrite

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 @ebevents = Unirest.get("https://www.eventbriteapi.com/v3/categories?&short_name&token=FHO43SJ3VXJC6S3P2TRR").body['categories'].each { |e| puts e.values[2] }
+# Eventbrite = GET /categories/¶
 
+# Chiketto::Category.list
+
+categories['categories'].map { |cat| Chiketto::Category.new cat }
 {"resource_uri"=>"https://www.eventbriteapi.com/v3/categories/103/", "id"=>"103", 
 "name"=>"Music", "name_localized"=>"Music", "short_name"=>"Music", "short_name_localized"=>"Music"}, 
 {"resource_uri"=>"https://www.eventbriteapi.com/v3/categories/101/", "id"=>"101", "name"=>"Business & Professional", 
 "name_localized"=>"Business & Professional", "short_name"=>"Business", "short_name_localized"=>"Business"}
+
+event = Chiketto::Event.find '22592779612'
+@events = Eventbrite::Event.search({q: 'dinosaurs'}, 'FHO43SJ3VXJC6S3P2TRR').events[0..3]
 # Next task is to seed my model 
